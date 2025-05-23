c7537b09a62856c3251fc6e14819ed7c965c996e
Mohammed Waqar <137026365+waqarbytes@users.noreply.github.com>
2025-05-23 10:28:32 +0530

    Add files via upload

---
+++ Src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import { ThemeProvider } from './contexts/ThemeContext.tsx';
+import './index.css';
+import './styles/theme.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
